5c719d6687921cfe90129c81c14f4350eb6ac094
Ehsan Hosseini <uni.ehsan.hosseini@gmail.com>
2023-05-16 13:02:54 +0330

    add Pr4

--- Pr4.py
+++ Pr4.py
@@ -5,10 +5,10 @@ f = open("Files/Pr4_project_data.csv" , "r" )
 text=f.read()
 f.close()
 # print(text)
-
+# quit()
 lines=text.split("\n")  
 # print(lines)
-
+# quit()
 
 table=[]        
 for line in lines:
@@ -18,8 +18,8 @@ for line in lines:
 
 
 table.pop(0)   
-print(table)
-quit()
+# print(table)
+# quit()
 
 
 values_columns=[]
@@ -27,11 +27,22 @@ for column in table:
     values_columns.append([int(column[1]),int(float(column[5]))])
 values_columns.reverse() 
 # print(values_columns)
-
+# quit()
 values_array=np.array(values_columns)
 values_close=values_array[:,1]
 days=values_array[:,0]
 days=days
-print(days)
+print(values_array)
+
+move_size=150
+moving_av=[]
+for i in range(len(values_close)-move_size):
+    values_mean=(values_close[i:i+move_size]).mean()
+    moving_av.append(values_mean)
+
+
+
+
 plt.plot(values_close)
+plt.plot(moving_av)
 plt.show()
